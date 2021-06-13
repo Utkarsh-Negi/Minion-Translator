59daa7afa22a4401adc15089ae3834c41829af4b
Utkarsh <utkarshnegi25@gmail.com>
2021-06-13 12:57:18 +0530

    Added event listener

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // input
-var username = prompt('give me username');
+// var username = prompt('give me username');
 
-// processing
-var welcomemessage = 'Working' + username;
+// // processing
+// var welcomemessage = 'Working' + username;
 
-// output
-alert(welcomemessage);
+// // output
+// alert(welcomemessage);
+
+var button = document.querySelector('#btn-translate');
+
+button.addEventListener('click', function clickEventHandler() {
+  console.log('clicked');
+});
