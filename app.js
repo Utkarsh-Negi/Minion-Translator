a1bd6a081aed686c90bacefc459c7675b5831dc3
Utkarsh <utkarshnegi25@gmail.com>
2021-06-14 14:05:59 +0530

    Added all the functionalities

--- app.js
+++ app.js
@@ -11,6 +11,26 @@ var button = document.querySelector('#btn-translate');
 var word = document.querySelector('#txt-input');
 var output = document.querySelector('#output');
 
+var serverURL = 'https://api.funtranslations.com/translate/minion.json';
+function getTranslationURL(text) {
+  return serverURL + '?' + 'text' + text;
+}
+
+function errorHandler(error) {
+  console.log('error occured', error);
+  alert(
+    'Something went wrong with the server please try again after sometime...'
+  );
+}
+
 button.addEventListener('click', function clickEventHandler() {
-  output.innerText = 'fdfscsdfcsdcsd' + word.value;
+  var inputText = word.value;
+
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      output.innerText = translatedText;
+    })
+    .catch(errorHandler);
 });
